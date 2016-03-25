a2afac678a07eb33f47bcefded2ea86e1a766a73
David Germain <User1@User1s-Mac-Pro.eau.wi.charter.com>
2016-03-24 23:08:59 -0400

    added custom voice over

--- app/src/main/java/com/example/user1/tellmetimer/MainActivity.java
+++ app/src/main/java/com/example/user1/tellmetimer/MainActivity.java
@@ -10,17 +10,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +34,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textToSpeech = new TextToSpeech(this, this);
+
         Timer clock = new Timer();
         // clock.onSecondTick(task);
         TimerTask task = new TimerTask() {
+
             TextView totalTime = (TextView) findViewById(R.id.total_time);
             TextView notificationCountDown = (TextView) findViewById(R.id.notification_count_down);
             final int ALARM_FREQUENCY_IN_SECONDS =  30;
@@ -46,9 +55,13 @@ public class MainActivity extends AppCompatActivity {
                         totalTime.setText(seconds + "");
                         notificationCountDown.setText(durationUntilNextAlarm + "");
                         seconds++;
-                        if (durationUntilNextAlarm == 0) {
+                        if (durationUntilNextAlarm == 1) {
                             //speak alarm
-                            soundAlarm();
+                            DateFormat dateFormat = new SimpleDateFormat("h:mm a");
+                            String text = "It is currently " + dateFormat.format(new Date()) +
+                                          "            " + //TODO add pause.
+                                          "This timer has been running for " + seconds + " seconds.";
+                            convertTextToSpeech(text);
                         }
 
                     }
@@ -57,6 +70,24 @@ public class MainActivity extends AppCompatActivity {
         };
 
         clock.scheduleAtFixedRate(task,0, 1000);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("error", "This Language is not supported");
+            } else {
+            }
+        } else {
+            Log.e("error", "Initilization Failed!");
+        }
+    }
+
+    private void convertTextToSpeech(String text) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 
     public void soundAlarm() {
