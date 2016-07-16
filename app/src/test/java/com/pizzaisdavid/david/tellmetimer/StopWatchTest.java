8180cbe346fa33dca3c9d989f19a47a3a8fd9ca9
David Germain <davidispizza@gmail.com>
2016-07-16 18:40:29 -0400

    Be able to run tests that have logs in them

--- app/src/test/java/com/pizzaisdavid/david/tellmetimer/StopWatchTest.java
+++ app/src/test/java/com/pizzaisdavid/david/tellmetimer/StopWatchTest.java
@@ -4,9 +4,13 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class StopWatchTest {
   StopWatch stopWatch;
   StopWatchSchedule mockStopWatchSchedule;
