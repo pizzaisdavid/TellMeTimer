24070f12df6b56be5f2107efcb46079c2de94e11
David Germain <davidispizza@gmail.com>
2016-07-16 14:29:25 -0400

    Fix test

--- app/src/test/java/com/pizzaisdavid/david/tellmetimer/TimePeriodTest.java
+++ app/src/test/java/com/pizzaisdavid/david/tellmetimer/TimePeriodTest.java
@@ -1,39 +1,15 @@
 package com.pizzaisdavid.david.tellmetimer;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
+public class TimePeriodTest {
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({TimePeriodTest.initializer.class, TimePeriodTest.tick.class})
-
-public class TimePeriodTest extends TestCase {
-
-  public static class initializer {
-
-    @Test
-    public void doubleZero() throws Exception {
-      //Assert.assertEquals(40, 9);
-    }
-
-    @Test
-    public void singleDigitSeconds() throws Exception {
-      //Assert.assertEquals(9, 9);
-    }
-  }
-
-  public static class tick {
-
-    @Test
-    public void run() throws Exception {
-      TimePeriod duration = new TimePeriod(20);
-      duration.tick();
-      Assert.assertEquals(21, duration.getSeconds());
-    }
-
+  @Test
+  public void run() throws Exception {
+    TimePeriod duration = new TimePeriod(20);
+    duration.tick();
+    Assert.assertEquals(21, duration.getSeconds());
   }
 }
