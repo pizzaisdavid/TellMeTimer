a9500c3c6ee96cfdb8fe056d85b1854eeb752f50
David Germain <davidispizza@gmail.com>
2016-07-16 17:35:41 -0400

    More tests

--- app/src/test/java/com/pizzaisdavid/david/tellmetimer/StopWatchTest.java
+++ app/src/test/java/com/pizzaisdavid/david/tellmetimer/StopWatchTest.java
@@ -4,9 +4,11 @@ import junit.framework.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class StopWatchTest {
+  StopWatch stopWatch;
   StopWatchSchedule mockStopWatchSchedule;
   UpdateTask mockUpdateTask;
 
@@ -18,6 +20,22 @@ public class StopWatchTest {
 
   @Test
   public void constructor() throws Exception {
-    StopWatch stopWatch = new StopWatch(mockStopWatchSchedule, mockUpdateTask);
+    stopWatch = new StopWatch(mockStopWatchSchedule, mockUpdateTask);
   }
+
+  @Test
+  public void setup() throws Exception {
+    stopWatch = new StopWatch(mockStopWatchSchedule, mockUpdateTask);
+    stopWatch.setup();
+    Mockito.verify(mockUpdateTask).pause();
+  }
+
+  @Test
+  public void resume() throws Exception {
+    stopWatch = new StopWatch(mockStopWatchSchedule, mockUpdateTask);
+    stopWatch.setup();
+    stopWatch.resume();
+    Mockito.verify(mockUpdateTask).resume();
+  }
+
 }
