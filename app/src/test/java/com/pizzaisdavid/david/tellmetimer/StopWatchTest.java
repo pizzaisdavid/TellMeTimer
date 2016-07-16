a9df891e4c22e89e57086b8979ff5f2599a48ceb
David Germain <davidispizza@gmail.com>
2016-07-16 15:04:20 -0400

    Setup test file for testing

--- app/src/test/java/com/pizzaisdavid/david/tellmetimer/StopWatchTest.java
+++ app/src/test/java/com/pizzaisdavid/david/tellmetimer/StopWatchTest.java
@@ -1,7 +1,23 @@
 package com.pizzaisdavid.david.tellmetimer;
 
-/**
- * Created by David on 7/16/16.
- */
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class StopWatchTest {
+  StopWatchSchedule mockStopWatchSchedule;
+  UpdateTask mockUpdateTask;
+
+  @Before
+  public void before() {
+    mockStopWatchSchedule = Mockito.mock(StopWatchSchedule.class);
+    mockUpdateTask = Mockito.mock(UpdateTask.class);
+  }
+
+  @Test
+  public void constructor() throws Exception {
+    StopWatch stopWatch = new StopWatch(mockStopWatchSchedule, mockUpdateTask);
+  }
 }
