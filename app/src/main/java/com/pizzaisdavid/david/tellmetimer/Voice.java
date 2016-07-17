6ac70f941f50148a76afad034e2ccdcb753c30be
David Germain <davidispizza@gmail.com>
2016-07-16 20:58:59 -0400

    Clean up

--- app/src/main/java/com/pizzaisdavid/david/tellmetimer/Voice.java
+++ app/src/main/java/com/pizzaisdavid/david/tellmetimer/Voice.java
@@ -1,14 +1,11 @@
 package com.pizzaisdavid.david.tellmetimer;
 
-import android.app.Activity;
 import android.speech.tts.TextToSpeech;
-import android.util.Log;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
-import java.util.Locale;
 
 public class Voice {
   private static final Logger logger = LoggerFactory.getLogger(Voice.class);
--- app/src/main/java/com/pizzaisdavid/david/tellmetimer/VoiceNotification.java
+++ app/src/main/java/com/pizzaisdavid/david/tellmetimer/VoiceNotification.java
@@ -1,38 +1,34 @@
 package com.pizzaisdavid.david.tellmetimer;
 
-import android.app.Activity;
 import android.speech.tts.TextToSpeech;
-
-import org.w3c.dom.Text;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class VoiceNotification extends Voice {
-
   private String queue;
 
   public VoiceNotification(TextToSpeech textToSpeech) {
     super(textToSpeech);
-    this.queue = "";
+    queue = "";
   }
 
   public void appendCurrentTimeToQueue() {
     DateFormat dateFormat = new SimpleDateFormat("h:mm a");
-    this.queue += " It is currently " + dateFormat.format(new Date()) + ".";
+    queue += " It is currently " + dateFormat.format(new Date()) + ".";
   }
 
   public void appendPauseToQueue() {
-    this.queue += "........"; // TODO attempt to take a breath between sentences.
+    queue += "..."; // TODO attempt to take a breath between sentences.
   }
 
   public void appendTotalTimeToQueue(TimePeriod duration) {
-    this.queue += " This timer has been running for " + TimePeriodFormat.simple(duration) + ".";
+    queue += " This timer has been running for " + TimePeriodFormat.simple(duration) + ".";
   }
 
   public void sayQueue() {
-    say(this.queue);
-    this.queue = "";
+    say(queue);
+    queue = "";
   }
 }
