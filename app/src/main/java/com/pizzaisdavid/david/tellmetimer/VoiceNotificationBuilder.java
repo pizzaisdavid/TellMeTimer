f1aaf293930654a8201d86464eaa6e23598e0bfd
David Germain <davidispizza@gmail.com>
2016-07-17 18:22:41 -0400

    Rename

--- app/src/main/java/com/pizzaisdavid/david/tellmetimer/VoiceNotificationBuilder.java
+++ app/src/main/java/com/pizzaisdavid/david/tellmetimer/VoiceNotificationBuilder.java
@@ -1,34 +1,36 @@
 package com.pizzaisdavid.david.tellmetimer;
 
-import android.speech.tts.TextToSpeech;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class VoiceNotification extends Voice {
+public class VoiceNotificationBuilder {
+  private static final Logger logger = LoggerFactory.getLogger(UpdateTask.class);
   private String queue;
 
-  public VoiceNotification(TextToSpeech textToSpeech) {
-    super(textToSpeech);
+  public VoiceNotificationBuilder() {
+    logger.debug("Initializing");
     queue = "";
   }
 
-  public void appendCurrentTimeToQueue() {
+  public void appendCurrentTime() {
     DateFormat dateFormat = new SimpleDateFormat("h:mm a");
     queue += " It is currently " + dateFormat.format(new Date()) + ".";
   }
 
-  public void appendPauseToQueue() {
+  public void appendPause() {
     queue += "..."; // TODO attempt to take a breath between sentences.
   }
 
-  public void appendTotalTimeToQueue(TimePeriod duration) {
+  public void appendDuration(TimePeriod duration) {
     queue += " This timer has been running for " + TimePeriodFormat.simple(duration) + ".";
   }
 
-  public void sayQueue() {
-    say(queue);
-    queue = "";
+  public String toString() {
+    logger.info("toString: {}", queue);
+    return queue;
   }
 }
